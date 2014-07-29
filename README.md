25416cb773787116d47dc0b0a6275a418c195f37
Alexis Wilpert <wilpert@StudioXPS-435mt.(none)>
2014-07-29 18:13:47 +0200

    first commit, still not complete, please be patient :-)

---
+++ README.md
@@ -0,0 +1,5 @@
+RusPhonetizer is a set of grammar rules and dictionaries for the phonetic transcription of Russian sentences.
+
+This is still work in progress. Especially, a script for wrapping up the functionality and documentation are still missing. Please, be patient. However, the main core - the grammar files with the FST rules for transcription -  are already here. I consider them to be the most valuable part of the whole package.
+
+Updates will follow soon.
---
+++ grammars/transduce.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright 2014 Yandex LLC
+# All Rights Reserved.
+#
+# Author : Alexis Wilpert
+
+
+
+if [ -z $1 ]
+then
+	echo -e "\nUsage: transduce.sh INPUT_FILE\n"
+	exit
+fi
+
+IN=$1
+OUT=$1.transduced
+
+rm -f transduce.stackdump
+farextract g2p.far
+
+transduce -fst="\
+READ,\
+INFL,\
+PALT,\
+DIPH,\
+VOWL,\
+CONS,\
+CROS,\
+SYLL,\
+WRIT\
+" $IN > $IN.g2p
